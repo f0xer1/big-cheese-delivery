f8eab2db51efc18095ec232f7995fa472dfa0b60
Pavlo Ilnitskyi <inavlo092@gmail.com>
2024-03-09 13:24:40 +0200

    initial commit

---
+++ src/main/java/com/bcd/big_cheese_delivery/BigCheeseDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.bcd.big_cheese_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigCheeseDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigCheeseDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bcd/big_cheese_delivery/BigCheeseDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcd.big_cheese_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigCheeseDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
