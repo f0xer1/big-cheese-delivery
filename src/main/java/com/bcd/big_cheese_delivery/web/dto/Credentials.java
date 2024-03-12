0cf783af3c4ba65a53066d517c496f69679684b9
Pavlo Ilnitskyi <inavlo092@gmail.com>
2024-03-12 19:30:27 +0200

    Add mapper, exception and dto

---
+++ src/main/java/com/bcd/big_cheese_delivery/web/dto/Credentials.java
@@ -0,0 +1,22 @@
+package com.bcd.big_cheese_delivery.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Credentials {
+    @NotBlank(message = "Specify username")
+    @Pattern(regexp = "^\\w+$", message = "You can use a-z, 0-9 and underscores")
+    @Size(min = 4, max = 32, message = "Enter at least 4 and less than 32 characters")
+    private String username;
+
+    @NotBlank(message = "Specify password")
+    @Size(min = 6, max = 32, message = "Enter at least 6 and less than 32 characters")
+    private String password;
+}
