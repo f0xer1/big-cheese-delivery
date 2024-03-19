d51529b4a4c423c976cab01970ea0c9f0e94edca
Bilokur Evgen <bilokur915@gmail.com>
2024-03-19 14:59:55 +0200

    Implemented testContainers

---
+++ src/test/java/com/bcd/big_cheese_delivery/utils/MongoRepositoryUtils.java
@@ -0,0 +1,11 @@
+package com.bcd.big_cheese_delivery.utils;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+@UtilityClass
+public class MongoRepositoryUtils {
+    public static <T, ID> T getById(ID id, MongoRepository<T, ID> mongoRepository) {
+        return mongoRepository.findById(id).orElseThrow(AssertionError::new);
+    }
+}
